beb9c9f7e83660406841f109299a92f81b8e6780
Cyrus Dang <cuong.dang@secpod.com>
2025-11-25 09:46:59 +0700

    feat(asset-discovery): add backend folder structure for asset_discovery

---
+++ src/backend/easm/apps/asset_discovery/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AssetDiscoveryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'easm.apps.asset_discovery'
+    verbose_name = 'Asset Discovery'
